8c437dadca08d596e2d4a0f1a4e2bb248a347bcd
AloisECN <marechalalois@yahoo.com>
2024-02-20 21:49:36 +0100

    song

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,31 @@ const App = () => {
     queryKey: ['tracks'],
     queryFn: fetchTracks
   });
+  console.log(tracks)
   const [trackIndex, setTrackIndex] = useState(0)
   const goToNextTrack = () => {
     setTrackIndex(trackIndex + 1);
   }
-
+  const AlbumCover = () =>  {
+    const src = tracks===undefined ? '' : tracks[trackIndex]?.track.album.images[0]?.url
+    return (
+        <img src={src} style={{ width: 400, height: 400 }} />
+    );
+}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Bienvenue sur le blind test {tracks===undefined ? '' : tracks.length} {tracks===undefined ? '' : tracks[0]?.track.name} {tracks===undefined ? '' : tracks[0]?.track.artists.map(artist => artist.name)} </h1>
+        <h1 className="App-title">Bienvenue sur le blind test {tracks===undefined ? '' : tracks.length} {tracks===undefined ? '' : tracks[0]?.track.name} {tracks===undefined ? '' : tracks[0]?.track.artists[0]?.name} </h1>
       </header>
       <div className="App-images">
         <p>Il va falloir modifier le code pour faire un vrai blind test</p>
       </div>
-      {/* <audio src={tracks[trackIndex]} autoPlay controls /> */}
+      <audio src={tracks===undefined ? '' : tracks[trackIndex]?.track.preview_url} autoPlay controls />
       <button onClick={goToNextTrack}>
        Next track
       </button>
+      <AlbumCover/>
       <div className="App-buttons"></div>
     </div>
   );
